1e3174ad79ba8d4dffa98e2ea28e097ce820bb6f
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2026-01-22 11:36:54 -0300

    feat: produto and categoria services methods created

--- src/categorias/services/categorias.service.ts
+++ src/categorias/services/categorias.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Categorias } from '../entities/categoria.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 
 @Injectable()
 export class CategoriasService {
@@ -9,4 +9,29 @@ export class CategoriasService {
     @InjectRepository(Categorias)
     private categoriasRepository: Repository<Categorias>,
   ) {}
+  async getAllcategories(): Promise<Categorias[]> {
+    return await this.categoriasRepository.find();
+  }
+  async getCategoryById(id: number): Promise<Categorias> {
+    const categoria = await this.categoriasRepository.findOne({
+      where: { id },
+    });
+    if (!categoria)
+      throw new HttpException(
+        'Categoria não encontrada!',
+        HttpStatus.NOT_FOUND,
+      );
+    return categoria;
+  }
+  async createCategory(categoria: Categorias): Promise<Categorias> {
+    return this.categoriasRepository.save(categoria);
+  }
+  async updateCategory(categoria: Categorias): Promise<Categorias> {
+    await this.getCategoryById(categoria.id);
+    return this.categoriasRepository.save(categoria);
+  }
+  async deleteCategory(id: number): Promise<DeleteResult> {
+    await this.getCategoryById(id);
+    return await this.categoriasRepository.delete(id);
+  }
 }
--- src/produtos/services/produtos.service.ts
+++ src/produtos/services/produtos.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Produtos } from '../entities/produto.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 
 @Injectable()
 export class ProdutosService {
@@ -9,4 +9,32 @@ export class ProdutosService {
     @InjectRepository(Produtos)
     private produtosRepository: Repository<Produtos>,
   ) {}
+  async getAllProducts(): Promise<Produtos[]> {
+    return await this.produtosRepository.find();
+  }
+  async getProductById(id: number): Promise<Produtos> {
+    const produto = await this.produtosRepository.findOne({
+      where: { id },
+    });
+    if (!produto)
+      throw new HttpException('Produto não encontrado!', HttpStatus.NOT_FOUND);
+    return produto;
+  }
+  async getDiscountByCarYear() {}
+  async getActiveProducts(): Promise<Produtos[]> {
+    return await this.produtosRepository.find({
+      where: { esta_ativo: true },
+    });
+  }
+  async createProduct(produto: Produtos): Promise<Produtos> {
+    return this.produtosRepository.save(produto);
+  }
+  async updateProduct(produto: Produtos): Promise<Produtos> {
+    await this.getProductById(produto.id);
+    return await this.produtosRepository.save(produto);
+  }
+  async deleteProduct(id: number): Promise<DeleteResult> {
+    await this.getProductById(id);
+    return this.produtosRepository.delete(id);
+  }
 }
