7686962637c46505926c45eb1d7bccb9b97383e6
Raylander Ribeiro (rayribeirost) <ribeiroraylander7@gmail.com>
2026-01-22 15:46:23 -0300

    fix: user service and controller fixed

--- src/produtos/controllers/produtos.controller.ts
+++ src/produtos/controllers/produtos.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ProdutosService } from '../services/produtos.service';
 import { Produtos } from '../entities/produto.entity';
 import { ProdutoComDescontoDTO } from '../dto/produto-com-desconto.dto';
@@ -7,46 +18,47 @@ import { ProdutoComDescontoDTO } from '../dto/produto-com-desconto.dto';
 export class ProdutosController {
   constructor(private readonly produtosService: ProdutosService) {}
 
-@Get()
+  @Get()
   @HttpCode(HttpStatus.OK)
   getAllProducts(): Promise<Produtos[]> {
     return this.produtosService.getAllProducts();
   }
-  
+
   @Get('/:id')
   @HttpCode(HttpStatus.OK)
   getProductById(@Param('id', ParseIntPipe) id: number): Promise<Produtos> {
     return this.produtosService.getProductById(id);
   }
 
-  @Get ('/descontos/:ano')
+  @Get('/descontos/:ano')
   @HttpCode(HttpStatus.OK)
-  getDiscountByCarYear(@Param('ano', ParseIntPipe) ano: number): Promise<ProdutoComDescontoDTO[]> {
+  getDiscountByCarYear(
+    @Param('ano', ParseIntPipe) ano: number,
+  ): Promise<ProdutoComDescontoDTO[]> {
     return this.produtosService.getDiscountByCarYear(ano);
-}
+  }
 
- @Get ('/ativos')
- @HttpCode(HttpStatus.OK)
- getActiveProducts():Promise<Produtos[]>  {
+  @Get('/ativos')
+  @HttpCode(HttpStatus.OK)
+  getActiveProducts(): Promise<Produtos[]> {
     return this.produtosService.getActiveProducts();
-}
+  }
 
-@Post()
+  @Post()
   @HttpCode(HttpStatus.CREATED)
- createProduct(@Body() produtos: Produtos): Promise<Produtos> {
+  createProduct(@Body() produtos: Produtos): Promise<Produtos> {
     return this.produtosService.createProduct(produtos);
   }
- 
-  
+
   @Put()
   @HttpCode(HttpStatus.OK)
-  updateProduct (@Body() produtos: Produtos): Promise<Produtos> {
-    return this.produtosService.updateProduct (produtos);
+  updateProduct(@Body() produtos: Produtos): Promise<Produtos> {
+    return this.produtosService.updateProduct(produtos);
   }
 
-@Delete('/:id')
+  @Delete('/:id')
   @HttpCode(HttpStatus.NO_CONTENT)
-  deleteProduct (@Param('id', ParseIntPipe) id: number){
-    return this.produtosService.deleteProduct (id);
+  deleteProduct(@Param('id', ParseIntPipe) id: number) {
+    return this.produtosService.deleteProduct(id);
   }
 }
